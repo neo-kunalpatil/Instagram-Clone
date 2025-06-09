92e67f2c30cabd0f03422a77703cdc054a3b1a3e
Kunalpatil3435@gmail.com <kunalpatil3435@gmail.com>
2025-06-09 10:00:55 +0530

    Initial commit

--- authy/apps.py
+++ authy/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class AuthyConfig(AppConfig):
     name = 'authy'
+    default_auto_field = 'django.db.models.BigAutoField'
--- notification/views.py
+++ notification/views.py
@@ -1,4 +1,4 @@
-import imp
+import importlib
 from django.shortcuts import render, redirect
 from notification.models import Notification
 
@@ -16,3 +16,9 @@ def DeleteNotification(request, noti_id):
     user = request.user
     Notification.objects.filter(id=noti_id, user=user).delete()
     return redirect('show-notification')
+
+def load_module_dynamically(module_name, file_path):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
